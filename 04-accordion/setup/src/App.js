baffa878b54c5817ff687463481d7692321d7ee4
webdevKmer <webdevKmer@gmail.com>
2023-06-30 06:29:06 +0000

    fix and finish accordion app

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+
+  const [questions, setQuestions] = useState(data)
+
+  return <main className="section">
+    <div className="container">
+    <h3>questions and answers about login</h3>
+    <div className="list">
+
+    {questions.map((question) => (
+      <SingleQuestion key={question.id} {...question}/>
+      ))}
+      </div>
+    
+    </div>
+  </main>;
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({title, info}) => {
+  const [show, setShow] = useState(false)
+  const toggleInfo = () => {
+    setShow((prev) => !prev)
+  }
+
+  return <div className="question">
+  <header>
+    <h4>{title}</h4>
+    <div className="btns">
+      <div onClick={toggleInfo} className={show ? 'btn, hide' : 'btn' }>
+        <AiOutlinePlus />
+      </div>
+      <div onClick={toggleInfo} className={show ? 'btn' : 'btn, hide'}>
+        <AiOutlineMinus/>
+      </div>
+    </div>
+  </header>
+  <p className={show ? undefined : 'hide'}>{info}</p>
+</div>
 };
 
 export default Question;
